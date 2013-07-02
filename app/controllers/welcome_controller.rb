0d7c2048fe71ad3ddd802f6d2b24b0e32016d975
Chandresh Pant <cp@cpant.in>
2013-07-02 05:58:21 +0530

    check for admin sample code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+
+    ## check for admin here  - remove comments:
+    # if current_user.admin?
+    @attendances = Attendance.order
+    # else
+    # @attendances = current_user.attendance
+
   end
 end
